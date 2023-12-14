fd6b10d0c191e409bf483e6f155e9fd286b79b83
CKirbs <ckggithub@gmail.com>
2023-12-14 18:47:41 +0800

    Calculation + Inputs Exceptions

--- TrigonometryCalc.py
+++ TrigonometryCalc.py
@@ -1,4 +1,5 @@
 import os
+import math
 
 # [A, B , C, ∠AC, ∠BC]
 var_values = ["N/A", "N/A", "N/A", "N/A", "N/A"]
@@ -17,7 +18,6 @@ display_print = f"""
 
 def display_guide():
     os.system("cls")
-    print(var_values)
     print(display_print)
     print(f"A = {var_values[0]}") if var_values[0] != "N/A" else print("", end="")
     print(f"B = {var_values[1]}") if var_values[1] != "N/A" else print("", end="")
@@ -33,7 +33,8 @@ def variable_available(var):
     try:
         if var_values[int(var)] == "N/A":
             return True
-    except:
+    except Exception as e:
+        print(e)
         return False
 
 
@@ -43,21 +44,82 @@ def assign_variables():
         variable = input(f"({i+1}) Given Variable: ")
     while True:
         try:
-            value = int(input(f"({i+1}) Value: "))
+            value = float(input(f"({i+1}) Value: "))
             if int(variable) == 3 or int(variable) == 4:  # if angles was chosen
-                var_values[3 if int(variable) == 4 else 4] = (
-                    90 - value
-                )  # Calculate for bot angles
+                if value >= 90:
+                    print("Angle Must Be Less Than 90")
+                    continue
+                var_values[3 if int(variable) == 4 else 4] = 90 - value
+
+            # Code below ensures hypotenuse is always longer than the two sides
+
+            # If side A has a value and hypotenuse is selected
+            if (var_values[0] != "N/A") and int(variable) == 2:
+                if var_values[0] >= value:
+                    print("Hypotenuse Must Be Longer Than Side A")
+                    continue
+            # If side B has a value and hypotenuse is selected
+            if (var_values[1] != "N/A") and int(variable) == 2:
+                if var_values[1] >= value:
+                    print("Hypotenuse Must Be Longer Than Side B")
+                    continue
+
+            # If hypotenuse has a value and side A is selected
+            if (var_values[2] != "N/A") and int(variable) == 0:
+                if var_values[2] <= value:
+                    print("Side A Must Be Shorter Than Hypotenuse")
+                    continue
+            # If hypotenuse has a value and side B is selected
+            if (var_values[2] != "N/A") and int(variable) == 1:
+                if var_values[2] <= value:
+                    print("Side B Must Be Shorter Than Hypotenuse")
+                    continue
+
             break
-        except:
+        except Exception as e:
             continue
-            pass
+
     var_values[int(variable)] = value
     display_guide()
 
 
+def is_not_NA(var1, var2):
+    if var_values[var1] == "N/A" or var_values[var2] == "N/A":
+        return False
+    return True
+
+
 def calculate_trigonometry():
-    pass
+    if is_not_NA(0, 1):
+        var_values[2] = round(math.sqrt(var_values[0] ** 2 + var_values[1] ** 2), 4)
+        var_values[3] = round(math.degrees(math.atan(var_values[1] / var_values[0])), 4)
+        var_values[4] = round(math.degrees(math.atan(var_values[0] / var_values[1])), 4)
+    elif is_not_NA(0, 2):
+        var_values[1] = round(math.sqrt(var_values[2] ** 2 - var_values[0] ** 2), 4)
+        var_values[3] = round(math.degrees(math.atan(var_values[1] / var_values[0])), 4)
+        var_values[4] = round(math.degrees(math.atan(var_values[0] / var_values[1])), 4)
+    elif is_not_NA(1, 2):
+        var_values[0] = round(math.sqrt(var_values[2] ** 2 - var_values[1] ** 2), 4)
+        var_values[3] = round(math.degrees(math.atan(var_values[1] / var_values[0])), 4)
+        var_values[4] = round(math.degrees(math.atan(var_values[0] / var_values[1])), 4)
+    elif is_not_NA(0, 3):
+        var_values[1] = round(var_values[0] * math.tan(math.radians(var_values[3])), 4)
+        var_values[2] = round(math.sqrt(var_values[0] ** 2 + var_values[1] ** 2), 4)
+    elif is_not_NA(1, 3):
+        var_values[0] = round(var_values[1] / math.tan(math.radians(var_values[3])), 4)
+        var_values[2] = round(math.sqrt(var_values[0] ** 2 + var_values[1] ** 2), 4)
+    elif is_not_NA(2, 3):
+        var_values[0] = round(var_values[2] * math.cos(math.radians(var_values[3])), 4)
+        var_values[1] = round(var_values[2] * math.sin(math.radians(var_values[3])), 4)
+    elif is_not_NA(0, 4):
+        var_values[1] = round(var_values[0] / math.tan(math.radians(var_values[4])), 4)
+        var_values[2] = round(math.sqrt(var_values[0] ** 2 + var_values[1] ** 2), 4)
+    elif is_not_NA(1, 4):
+        var_values[0] = round(var_values[1] * math.tan(math.radians(var_values[4])), 4)
+        var_values[2] = round(math.sqrt(var_values[0] ** 2 + var_values[1] ** 2), 4)
+    elif is_not_NA(2, 4):
+        var_values[0] = round(var_values[2] * math.sin(math.radians(var_values[4])), 4)
+        var_values[1] = round(var_values[2] * math.cos(math.radians(var_values[4])), 4)
 
 
 display_guide()
@@ -65,3 +127,12 @@ given_variables = []
 for i in range(2):
     assign_variables()
 calculate_trigonometry()
+display_guide()
+
+""" 
+A = 3.0
+B = 4.0
+C = 5.0
+∠AC = 53.13°
+∠BC = 36.87° 
+"""
